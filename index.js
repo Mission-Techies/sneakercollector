4d255dcead4434565b01411d03cb4e6e31092b46
Yuridia Larios Aispuro <yuriydani@gmail.com>
2016-11-30 22:31:51 -0800

    Manually updated index.js
    rodriguez415:~/workspace (master) $ git push
    Warning: Permanently added 'github.com,192.30.253.113' (RSA) to the list of known hosts.
    ERROR: Permission to Mission-Techies/sneakercollector.git denied to rodriguez415.
    fatal: Could not read from remote repository.

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
-
 /*global $*/
-var all_murals= 'https://api.airtable.com/v0/appC2gzawbLgSvOeE/Art?api_key=' + api_key;
+var api_key = 'keyn9ppa2rAt3Dbov';
+
+var shoes_records = 'https://api.airtable.com/v0/appSGsFkaru4JJ41z/Shoes?api_key=' + api_key;
+
+
+
 // All details
-function renderRecords(data) {
-$(data.records).each(function(index, mural) {
-var mural_name = mural.fields['Name/Description'];
-var mural_where = mural.fields['Where'];
-var mural_pics = mural.fields['Pic'];
-var mural_info = '';
-if (mural_name) {
-mural_info +=`<div class="media">`;
-mural_info +=`<div class="media-left">`;
-if (mural_pics) {
-$.each(mural_pics, function(i, pic){
-mural_info +=`<a href="detail.html?muralID=${mural.id}"><img src="${pic.url}"></a>`;
-});
-}
-mural_info +=`</div>`;
-mural_info += `<div class="media-body">${mural_name}</div>`;
-mural_info +=`</div>`;
-}
-$('.murals').append(mural_info);
-});
-}
-$.get(all_murals, renderRecords);
+function shoesRecords(data) 
+{
+  
+    $(data.records).each(function(index, shoes) {
+      var shoe_name = shoes.fields['Name'];
 
-    Contact GitHub
-    API
-    Training
-    Shop
-    Blog
-    About
+      var shoe_pics = shoes.fields['Pictures'];
+      var shoe_foams = shoes.fields['foamposites'];
+      
+      var shoe_info = '';
+      if (shoe_name && shoe_foams) {
+        shoe_info +=`<div class="media">`;
+          shoe_info +=`<div class="column">`;
+        shoe_info += `<div class="col-sm-6 col-md-4"`;
+          if (shoe_pics) {
+            $.each(shoe_pics, function(i, pic){
+              shoe_info +=`<a href="detail.html?muralID=${shoes.id}"><img src="${pic.url}"></a>`;
+            });
+          }
+          shoe_info +=`</div>`;
+        shoe_info += `<div class="media-body">${shoe_name}</div>`;
+        shoe_info +=`</div>`;
+      }
+      
+      $('.shoes_foams').append(shoe_info);
+    });
+}
 
+$.get(shoes_records, shoesRecords);
